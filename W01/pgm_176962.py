30d52cee81e327a705080b8f3030e2eb8c84e0ec
yeonsu98 <yunsu9888@gmail.com>
2023-04-19 16:36:21 +0900

    다른 방식으로 풀이

--- W01/pgm_176962.py
+++ W01/pgm_176962.py
@@ -9,24 +9,17 @@ def solution(plans):
         plan[1] = time[0]*60 + time[1] # start
         plan[2] = int(plan[2])         # playtime
 
-    stop = []             # 중단된 과제 
-    current = plans[0][1] # 현재 시간
+    stop = []  # 중단된 과제 
     
-    for i in range(len(plans)-1):
-        np, _, tp = plans.pop(0)
-        stop.append([np, tp])
+    for plan in plans:
+        name, start, play = plan
         
-        while stop:
-            ns, ts = stop.pop()
-            if current + ts <= plans[0][1]:
-                answer.append(ns)
-                current += ts
-            else:
-                res = (current + ts) - plans[0][1] # 남은 시간
-                current += ts - res
-                stop.append([ns, res])
-                break
+        while stop and stop[-1][1] <= start:
+            answer.append(stop.pop()[0])
+        stop.append([name, start])
         
-    answer.append(plans[-1][0])
+        for s in stop: 
+            s[1] += play
+    
     answer.extend(list(s[0] for s in stop[::-1]))
     return answer
